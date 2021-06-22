5042b7a8b408bfb8b927af252460b96fad975aaf
takagi <iam.takagi.dev@gmail.com>
2021-06-22 11:45:35 +0900

    fix: header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,49 +16,6 @@ class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <meta charSet="utf-8" />
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;800&display=swap"
-            rel="stylesheet"
-          />
-
-          <title>iamtakagi.net</title>
-          <meta property="og:title" content="iamtakagi.net" />
-          <meta property="og:description" content="iamtakagi.netu" />
-          <meta property="og:url" content="https://iamtakagi.net/" />
-          <meta property="og:image" content="https://iamtakagi.net/icon.png" />
-          <meta name="twitter:card" content="summary" />
-          <meta name="twitter:title" content="iamtakagi.net" />
-          <meta name="twitter:description" content="iamtakagi.net" />
-          <meta name="twitter:site" content="@iam_takagi" />
-
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-          <meta name="msapplication-TileColor" content="#da532c" />
-          <meta name="theme-color" content="#ffffff" />
-        </Head>
         <body className="min-h-screen">
           <Main />
           <NextScript />
