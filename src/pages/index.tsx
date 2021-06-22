80a8990310a260cbd3805cdc65203b24dc3e0429
takagi <iam.takagi.dev@gmail.com>
2021-06-23 01:57:34 +0900

    cleanup: components structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,18 @@
 import React from "react";
+
+import { Top } from "../components/contents/Top";
 import { Accounts } from "../components/contents/Accounts";
 import { Developments } from "../components/contents/Developments";
+import { Contact } from "../components/contents/Contact";
 
 const IndexPage: React.VFC<{}> = () => {
   return (
-    <div>
-      <div className="desc w-full my-6 space-y-1 text-left">
-        <p className="text-sm">プログラミングとゲームとアニメと音楽が好き。</p>
-      </div>
-
+    <>
+      <Top />
       <Accounts />
       <Developments />
-
-      <div className="desc w-full my-6 space-y-1 text-left">
-        <p className="text-lg">Contact / 連絡</p>
-        <p className="text-sm">
-          ご依頼承ります。依頼の内容によってはお断りさせて頂く場合もございますが、お気軽にご相談ください。
-        </p>
-        <p className="text-sm">E-Mail: iamtakagi.contact@gmail.com</p>
-      </div>
-    </div>
+      <Contact />
+    </>
   );
 };
 
