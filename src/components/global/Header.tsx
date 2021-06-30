9acd3cf13c7f0147421422b2e97fd1e43ae141ac
takagi <iam.takagi.dev@gmail.com>
2021-07-01 04:43:29 +0900

    fix: flex

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -4,15 +4,16 @@ import ThemeSwitch from "../widgets/ThemeSwitch";
 
 export const Header: React.VFC<{}> = () => {
   return (
-    <header>
-      <div className="mt-5">
-        <ThemeSwitch />
-      </div>
-
+    <header className="flex">
       <div className="desc w-full my-6 space-y-1">
         <p className="text-xl">iamtakagi.net</p>
         <p className="text-sm">Welcome. (⋈◍＞◡＜◍)。✧♡</p>
       </div>
+
+      <div className="mt-5">
+        <ThemeSwitch />
+      </div>
+
     </header>
   );
 };
