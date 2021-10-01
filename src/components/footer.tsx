4ac804057c3f57102c469f6fa525dbd0be433298
takagi <iam.takagi.dev@gmail.com>
2021-10-02 05:18:43 +0900

    Remove github link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,14 +4,7 @@ import { FiGithub } from "react-icons/fi";
 export const Footer: React.FC<{}> = () => {
   return (
     <footer className="desc w-full my-6 space-y-1 mt-7">
-      <p className="text-sm">© 2021- takagi</p>
-      <a
-        className="text-sm underline"
-        href="https://github.com/iamtakagi-net/iamtakagi.net"
-      >
-        View on GitHub
-        <FiGithub className="inline block ml-1" />
-      </a>
+      <p className="text-sm">© 2012- takagi</p>
     </footer>
   );
 };
